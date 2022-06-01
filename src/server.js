0e686bd3c9774755804c5597f8c20f07fd9a008f
tql054 <tuanquocle2001@gmail.com>
2022-06-02 00:39:15 +0700

    commit add cors policy

--- src/server.js
+++ src/server.js
@@ -3,24 +3,23 @@ import bodyParser from "body-parser"
 import viewEngie from "./config/viewEngine"
 import initWebRoutes from "./route/web"
 import connectDB from './config/connectDB'
+import cors from 'cors'
 require('dotenv').config()
 let app = express()
-
+app.use(cors({origin: true, credentials: true}));
 //config app
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 viewEngie(app)
 initWebRoutes(app)
+// This should already be declared in your API file
+// var app = express();
 
 // ADD THIS
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.header("Access-Control-Allow-Headers", "x-access-token, Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
-connectDB()
+// var cors = require('cors');
+// app.use(cors());
+// connectDB()
 
 let port = process.env.PORT || 6969
 
